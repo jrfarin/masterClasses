d9c1cbd56a47e8eaed10b03ccf099dfde586b7c8
jrfarin <pepfarin@gmail.com>
2017-09-19 20:51:48 +0200

    Router Fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
+import { RecetasComponent } from './components/recetas/recetas.component';
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'recetas', component: RecetasComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
